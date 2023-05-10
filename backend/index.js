ad3995adb38e76dbd1d61bb0f6f5b9eaf9971185
dickenzoh <dickenzoh@gmail.com>
2023-05-10 16:44:44 +0300

    added database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,3 +14,18 @@ app.use(cors());
 app.listen(8800, () => {
   console.log("Hello from fiver clone API art by dickenzoh");
 });
+
+const PORT = process.env.PORT || 3005;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_CONN_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
