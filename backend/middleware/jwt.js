b0324a95a9197dd279438ecf8d3c96f3e8c0db82
dickenzoh <dickenzoh@gmail.com>
2023-05-13 04:43:04 +0300

    bootstraped the frontend

--- backend/middleware/jwt.js
+++ backend/middleware/jwt.js
@@ -2,8 +2,11 @@ import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
 export const verifyToken = (req, res, next) => {
+  console.log(req.Cookies);
+  if (!req.cookies || !req.cookies.accessToken) {
+    return next(createError(401, "You are not authenticated now!"));
+  }
   const token = req.cookies.accessToken;
-  if (!token) return next(createError(401, "You are not authenticated!"));
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return next(createError(403, "Token is not valid!"));
